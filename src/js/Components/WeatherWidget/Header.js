cbc85d50cbf864d693e4d39d6ac26b802b1a3168
vikspan <viktorija.span@gmail.com>
2025-07-25 16:30:10 +0200

    add header widget

--- src/js/Components/WeatherWidget/Header.js
+++ src/js/Components/WeatherWidget/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return (
         <div className="widget__header">
             <h1 className="widget__header--city" >Ljubljana</h1>
-            <div className="widget__header--temp">20 C</div>
+            <div className="widget__header--temp">20°C</div>
             <div className="widget__header--state">Clear</div>
         </div>
     )
