c029990820b23bbc29ba6da81d016cfc900c3a82
vikspan <viktorija.span@gmail.com>
2025-07-25 13:30:29 +0200

    setting up the environment

---
+++ src/js/Components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../styles/main.scss";
+
+const App = () => {
+    return (
+        <>Test</>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const entryPath = "src";
+
+module.exports = {
+  mode: "none",
+  entry: `./${entryPath}/js/index.js`,
+  devtool: "inline-source-map",
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, `${entryPath}/build`),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    static: [
+      {
+        directory: path.join(__dirname, entryPath),
+        publicPath: "/",
+        serveIndex: true,
+      },
+    ],
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    compress: true,
+    port: 3001,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['autoprefixer']],
+              },
+            },
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "/images/",
+          outputPath: "/images/"
+        }
+      }
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
