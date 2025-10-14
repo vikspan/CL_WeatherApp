f81172c725d3e25f421b44ee09667c30d5e25cd4
vikspan <viktorija.span@gmail.com>
2025-10-14 20:06:50 +0200

    Stable production build: all assets load correctly, source maps ignored

--- src/js/Components/WeatherWidget/HourlyForecast/HourlyForecast.js
+++ src/js/Components/WeatherWidget/HourlyForecast/HourlyForecast.js
@@ -1,5 +1,16 @@
 import React from "react";
 import styles from "./HourlyForecast.scss";
+import sunny from "../../../../assets/img/sunny.png";
+import cloudy from "../../../../assets/img/cloudy.png";
+import rainy from "../../../../assets/img/rainy.png";
+import night from "../../../../assets/img/night.png";
+
+const stateIcons = {
+  sunny,
+  cloudy,
+  rainy,
+  night
+};
 
 const HourlyForecast = (props) => {
   const { forecast } = props;
@@ -8,11 +19,10 @@ const HourlyForecast = (props) => {
     <div className="hourly__forecast">
       {
         forecast.map((hour, i) => {
-          const stateImg = `../../../assets/img/${hour.state}.png`;
           return (
             <div key={i} className="hourly__forecast--item">
               <span>{hour.hour}</span>
-              <img src={stateImg} />
+              <img src={stateIcons[hour.state]} alt={hour.state} />
               <span>{hour.temperature}°C</span>
             </div>
           )
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import bgImage from "../assets/img/bg.png";
+import "../styles/main.scss";
 import App from "./Components/App";
+
+document.body.style.backgroundImage = `url(${bgImage})`;
 
 const container = document.getElementById("app");
 const root = createRoot(container);
